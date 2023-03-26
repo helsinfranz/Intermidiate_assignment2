98cc6e2ed346da574f7b390d374c3f672bc5595d
helsinfranz <88080283+helsinfranz@users.noreply.github.com>
2023-03-26 19:15:46 +0530

    Create contract.sol

---
+++ contract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+contract newone{
+    function sum(uint a,uint b)public pure returns(uint){
+        return a+b;
+    }
+    function sub(uint a,uint b)public pure returns(uint){
+        return a-b;
+    }
+    function mul(uint a,uint b)public pure returns(uint){
+        return a*b;
+    }
+}
