c0b2ededfea558b42e60edfa39722b33095d2571
helsinfranz <88080283+helsinfranz@users.noreply.github.com>
2023-03-27 15:36:22 +0530

    Update BankApp.js

--- react_code/src/BankApp.js
+++ react_code/src/BankApp.js
@@ -7,20 +7,19 @@ import Interactions from './Interactions';
 const BankApp = () => {
 
 	// deploy simple token contract and paste deployed contract address here. This value is local ganache chain
-	let contractAddress = '0x59eFE99aA926a79edEA31F7ED3b2661b1F9e2F62';
+	let contractAddress = '0x75fCfC95271e0DdbeffC07be2957680685E7D225';
 
 	const [errorMessage, setErrorMessage] = useState(null);
 	const [defaultAccount, setDefaultAccount] = useState(null);
 	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+	const [sum, setSum] = useState("");
+	const [sub, setSub] = useState("");
+	const [mul, setMul] = useState("");
 
 	const [provider, setProvider] = useState(null);
 	const [signer, setSigner] = useState(null);
 	const [contract, setContract] = useState(null);
-
-	const [transferHash, setTransferHash] = useState(null);
-	const [checkAcc, setCheckAcc] = useState("false");
 	const [accStatus, setAccStatus]= useState("");
-	const [accbalance, setAccBalance]= useState("");
 
 	const connectWalletHandler = () => {
 		if (window.ethereum && window.ethereum.isMetaMask) {
@@ -32,7 +31,6 @@ const BankApp = () => {
 			})
 			.catch(error => {
 				setErrorMessage(error.message);
-			
 			});
 
 		} else {
@@ -66,101 +64,67 @@ const BankApp = () => {
 		let tempContract = new ethers.Contract(contractAddress, simple_token_abi, tempSigner);
 		setContract(tempContract);	
 	}
-
-	const createAccount = async()=>{
-		let txt = await contract.createAcc();
-		console.log(txt);
-		setAccStatus("Your Account is created");
-	}
-	const checkAccountExists = async()=>{
-		///e.preventDefault();
-		let txt = await contract.accountExists();
-		if(txt==true){
-		setCheckAcc("true");
-		}
-	}
-	const AccountBalance = async()=>{
-		let txt= await contract.accountBalance();
-		let balanceNumber = txt.toNumber();
-		//let tokenDecimals = await contract.decimals();
-		console.log(balanceNumber)
-		setAccBalance(''+balanceNumber);
-	}
-	const DepositBalance = async(e)=>{
+	const Addition = async(e)=>{
 		e.preventDefault();
-		let depositAmount = e.target.depositAmount.value;
-		let txt= await contract.deposit({
-			value: depositAmount 
-		});
+		let a = e.target.add1.value;
+		let b = e.target.add2.value;
+		let txt= await contract.sum(a,b);
+		setSum(parseInt(txt));
 	}
-	const transferHandler = async (e) => {
+	const Substraction = async(e)=>{
 		e.preventDefault();
-		let transferAmount = e.target.sendAmount.value;
-		let recieverAddress = e.target.recieverAddress.value;
-		let txt = await contract.transferEther(recieverAddress, transferAmount);
-		setTransferHash("Transfer confirmation hash: " + txt.hash);
+		let a = e.target.sub1.value;
+		let b = e.target.sub2.value;
+		let txt= await contract.sub(a,b);
+		setSub(parseInt(txt));
 	}
-	const WithdrawBalance = async(e)=>{
+	const Multiplication = async(e)=>{
 		e.preventDefault();
-		let withdrawAmount = e.target.withdrawAmount.value;
-		let txt= await contract.withdraw(withdrawAmount);
-		console.log(txt);
+		let a = e.target.mul1.value;
+		let b = e.target.mul2.value;
+		let txt= await contract.mul(a,b);
+		setMul(parseInt(txt));
 	}
 
 
 	return (
 	<div >
-		<h2> Bank Dapp- Create Account,Check Account,Check Balance,Transfer and Deposit </h2>
+		<h2> Calculations Dapp- Addition, Substraction, Multiplication </h2>
 		<button className={styles.button6} onClick={connectWalletHandler}>{connButtonText}</button>
-
-		<div className={styles.walletCard}>
-		<div>
-			<h3>Address: {defaultAccount}</h3>
-		</div>
-
-		<div>
-			<button onClick={AccountBalance}>Account Balance</button> <h3>Balance in the Bank: {accbalance} </h3>
-		</div>
-
-			{errorMessage}
-		</div>
+		<h3>Address: {defaultAccount}</h3>
+		{errorMessage}
 		<div className={styles.interactionsCard}>
-		<div>
-		<h4>Click here to Create your account. Make sure you are connected to Wallet</h4>
-			<button onClick={createAccount}>CreateAccount</button>
-			<h4>Click here to check if your account Exists or not</h4>
-			<button onClick={checkAccountExists}>CheckAccountExists</button>
-			<h4>Your Account Status</h4> <h5> {checkAcc}</h5>
-		</div>
-				<form onSubmit={transferHandler}>
-					<h3> Transfer money </h3>
-						<p> Reciever Address </p>
-						<input type='text' id='recieverAddress' className={styles.addressInput}/>
-
-						<p> Transfer Amount </p>
-						<input type='number' id='sendAmount' min='0' step='1'/>
-
-						<button type='submit' className={styles.button6}>Transfer</button>
-						<div>
-							{transferHash}
-						</div>
+			<form onSubmit={Addition}>
+					<h3> Addition </h3>
+					<div className={styles.form_field}>
+						<input type='number' id='add1' min='0' step='1'/>
+						<p> + </p>
+						<input type='number' id='add2' min='0' step='1'/>
+						<button type='submit' className={styles.button6}>ADD</button>
+					<h3> = {sum}</h3>
+					</div>
 			</form>
-			<form onSubmit={DepositBalance}>
-					<h3> Deposit Money </h3>
-						<p> Deposit Amount </p>
-						<input type='number' id='depositAmount' min='0' step='1'/>
-						<button type='submit' className={styles.button6}>Deposit</button>
-
+			<form onSubmit={Substraction}>
+					<h3> Substraction </h3>
+					<div className={styles.form_field}>
+						<input type='number' id='sub1' min='0' step='1'/>
+						<p> - </p>
+						<input type='number' id='sub2' min='0' step='1'/>
+						<button type='submit' className={styles.button6}>SUB</button>
+					<h3> = {sub}</h3>
+					</div>
 			</form>
-			<form onSubmit={WithdrawBalance}>
-					<h3> Withdraw Money </h3>
-						<p>Withdraw Amount </p>
-						<input type='number' id='withdrawAmount' min='0' step='1'/>
-						<button type='submit' className={styles.button6}>Withdraw</button>
-
+			<form onSubmit={Multiplication}>
+					<h3> Multiplication </h3>
+					<div className={styles.form_field}>
+						<input type='number' id='mul1' min='0' step='1'/>
+						<p> X </p>
+						<input type='number' id='mul2' min='0' step='1'/>
+						<button type='submit' className={styles.button6}>MUL</button>
+					<h3> = {mul}</h3>
+					</div>
 			</form>
 		</div>
-
 	</div>
 	)
 }
